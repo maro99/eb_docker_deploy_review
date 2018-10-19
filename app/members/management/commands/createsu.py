a31fbbb7f2ca6b9425a53f480ea998d68888d696
nadcdc <nadcdc4@gmail.com>
2018-10-19 19:54:08 +0900

    1.UserManager 를 기존 AbstractUser의 Usermanager를 오버라이딩 해놓음/ 2. Json파일 이용해서 createsu.py 만들어서 command등록 -->  superuser생성

---
+++ app/members/management/commands/createsu.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+
+from config.settings.base import SECRET_DIR
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **option):
+        secrets = json.load(open(os.path.join(SECRET_DIR, 'base.json')))
+        if not User.objects.filter(username=secrets['SUPERUSER_USERNAME']).exists():
+            User.objects.create_superuser(
+                username=secrets['SUPERUSER_USERNAME'],
+                password=secrets['SUPERUSER_PASSWORD'],
+                email=secrets['SUPERUSER_EMAIL'],
+            )
