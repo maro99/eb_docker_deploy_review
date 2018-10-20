70f0c3a0cc6703fd445583e1dfced2a9c970729f
nadcdc <nadcdc4@gmail.com>
2018-10-20 19:00:41 +0900

    dockerhub에 올라간 base이미지와 deploy.sh에서 requriement저장하는부분 중복되서 deploy.sh에서 삭제함.
    deploy.sh 에 쓸때없이 requirements.txt 만들어주는데
    생각해보니 그런 과정 Dockerfile.base에 다 있는 내용이고.(우리는 도커헙에서 다 가져다쓰는중.)
    Dockerfile에서 추가적으로 requriements만들고 install해주는 과정이 없다 .
    deploy.sh에서 제거해주자.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# requirements 만들기
-pipenv lock --requirements > requirements.txt
-
-# .secrets와 requirements를 staging area에 추가
-git add -f .secrets/ requirements.txt
+# .secrets staging area에 추가
+git add -f .secrets
 
 # eb deploy 실행
 eb deploy --profile fc-8th-eb --staged
 
-# .secrets와 requirements를 staging area에서 제거
-git reset HEAD .secrets/ requirements.txt
+# .secrets staging area에서 제거
+git reset HEAD .secrets
 
-# requirements.txt 삭제
-rm requirements.txt
