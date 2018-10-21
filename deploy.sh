698c650204ace5dffd389a06e0276b0c485ef167
nadcdc <nadcdc4@gmail.com>
2018-10-22 00:46:38 +0900

    http요청시 https로 redirect설정 / deploy시 변경사항 모두 add 하고 reset하도록 함

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # .secrets staging area에 추가
+git add -A
 git add -f .secrets
 
 # eb deploy 실행
@@ -8,4 +9,5 @@ eb deploy --profile fc-8th-eb --staged
 
 # .secrets staging area에서 제거
 git reset HEAD .secrets
+git reset
 
